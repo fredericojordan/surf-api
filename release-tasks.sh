3d9e7f1a804275a4fe04c79e42df57c91fd6cb73
Frederico Jordan <fredericojordan@gmail.com>
2019-05-19 23:33:46 -0300

    Fixing release tasks

--- release-tasks.sh
+++ release-tasks.sh
@@ -3,7 +3,11 @@ mix deps.get --only prod
 mix compile
 
 # Compile assets
-cd assets && webpack --mode production && cd ..
+cd assets
+npm install
+webpack --mode production
+cd ..
+
 mix phx.digest
 
 # Custom tasks (like DB migrations)
