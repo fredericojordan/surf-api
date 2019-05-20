8c2319c53b6615ff094b7398f167cc67d0672cc1
Frederico Jordan <fredericojordan@gmail.com>
2019-05-20 00:14:25 -0300

    Adding priv/static

--- release-tasks.sh
+++ release-tasks.sh
@@ -2,14 +2,5 @@
 mix deps.get --only prod
 mix compile
 
-# Compile assets
-cd assets
-npm install
-npm install -g webpack webpack-cli
-webpack-cli --mode production
-cd ..
-
-mix phx.digest
-
 # Custom tasks (like DB migrations)
 mix ecto.migrate
