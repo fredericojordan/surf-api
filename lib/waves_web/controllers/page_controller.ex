8c96cc9e569d2b70d0f5cffbafa4c124e726aef1
Frederico Jordan <fredericojordan@gmail.com>
2019-05-19 22:16:19 -0300

    Fixing PageController

--- lib/waves_web/controllers/page_controller.ex
+++ lib/waves_web/controllers/page_controller.ex
@@ -11,23 +11,33 @@ defmodule WavesWeb.PageController do
       |> first
       |> Repo.one()
 
-    wave_height = [
-      %{
-        "name":  "Wave Height",
-        "data": Map.new(List.zip([forecast.datetimes, forecast.wave_height]))
-      },
-    ]
+    wave_height =
+      case forecast do
+        nil -> []
+        _ ->
+          [
+            %{
+              "name":  "Wave Height",
+              "data": Map.new(List.zip([forecast.datetimes, forecast.wave_height]))
+            },
+          ]
+      end
 
-    wind_data = [
-      %{
-        "name":  "Wind Speed",
-        "data": Map.new(List.zip([forecast.datetimes, forecast.wind_speed]))
-      },
-      %{
-        "name":  "Wind Gusts",
-        "data": Map.new(List.zip([forecast.datetimes, forecast.wind_gust]))
-      },
-    ]
+    wind_data =
+      case forecast do
+        nil -> []
+        _ ->
+          [
+            %{
+              "name":  "Wind Speed",
+              "data": Map.new(List.zip([forecast.datetimes, forecast.wind_speed]))
+            },
+            %{
+              "name":  "Wind Gusts",
+              "data": Map.new(List.zip([forecast.datetimes, forecast.wind_gust]))
+            },
+          ]
+      end
 
     conn
     |> assign(:wave_height, wave_height)
